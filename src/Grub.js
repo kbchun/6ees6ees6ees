818e17a219a3880d92729d0a00199eeddefe1326
Kyle Chun <kbchun5712@gmail.com>
2016-09-20 15:55:37 -0700

    Pass all tests

--- src/Grub.js
+++ src/Grub.js
@@ -1,4 +1,14 @@
 class Grub {
+  constructor() {
+    this.age = 0;
+    this.color = 'pink';
+    this.food = 'jelly';
+    // this.eat = function();
+  }
+
+  eat() {
+    return 'Mmmmmmmmm jelly';
+  }
 };
 
 export default Grub;
